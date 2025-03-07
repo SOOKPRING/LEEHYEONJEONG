39b576690f8f68de84ddba2a4ee1aa18d2586bfa
hyhy-j <lhjlhj10088@gmail.com>
2025-03-07 17:22:16 +0900

    [Settings] Springboot Project Settings

---
+++ src/test/java/hello2/hello2_spring/Hello2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hello2.hello2_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hello2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
