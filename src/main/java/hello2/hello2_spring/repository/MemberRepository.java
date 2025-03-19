ffe49ef03e722d27d4cee445d965641a67c8665d
hyhy-j <lhjlhj10088@gmail.com>
2025-03-19 11:04:26 +0900

    [Feat] MemberRepository interface 구현

---
+++ src/main/java/hello2/hello2_spring/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package hello2.hello2_spring.repository;
+
+import hello2.hello2_spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+
+    Member save(Member member);
+
+    Optional<Member> findById(Long id);
+
+    Optional<Member> findByName(String name);
+
+    List<Member> findAll();
+}
